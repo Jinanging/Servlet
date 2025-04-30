9c24fd4faca73d57f7c5f75cf07b584c830e8d09
megait202 <jinan@gmail.com>
2025-04-30 18:45:25 +0900

    [권진환] -post method 일부 예제 작성

---
+++ src/main/java/com/jinanging/servlet/ex/Ex05Controller.java
@@ -0,0 +1,20 @@
+package com.jinanging.servlet.ex;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Ex05Controller extends HttpServlet{
+	
+	public void doPost(HttpServletRequest request,HttpServletResponse response) {
+		
+		response.setCharacterEncoding("utf-8");
+		response.setContentType("text/plain");
+		
+		//이름과 생년월일을 전달받고,
+		//이름과 나이를 html로 구성
+		
+		
+	}
+
+}
